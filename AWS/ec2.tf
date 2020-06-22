433f058d4a72542e293471e8d35b103af2a1af32
muskan399 <muskanmodi962@gmail.com>
2020-06-22 09:16:46 +0530

    Using file system for storage

--- AWS/ec2.tf
+++ AWS/ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 }
 
 resource "aws_security_group" "terrasg1" {
+
   name        = "terrasg1"
   description = "allow ssh and http traffic"
 
@@ -22,6 +23,12 @@ resource "aws_security_group" "terrasg1" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port       = 0
@@ -29,6 +36,21 @@ resource "aws_security_group" "terrasg1" {
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
   }
+}
+resource "aws_security_group" "terrasg2" {
+
+  name        = "terrasg2"
+  description = "allow nfs"
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  
 }
 resource "tls_private_key" "terrakey-new" {
   algorithm = "RSA"
@@ -41,6 +63,9 @@ resource "aws_key_pair" "generated_key" {
   //Using unique id of above
 
   public_key = "${tls_private_key.terrakey-new.public_key_openssh}"
+}
+output "out2" {
+value=tls_private_key.terrakey-new.private_key_pem
 }
 resource "aws_instance" "terraos-new1" {
   ami               = "ami-5b673c34"
@@ -56,7 +81,7 @@ connection {
   }
     inline =[
       "sudo yum install httpd -y",
-      "sudo yum install git -y" ,
+    
       "sudo systemctl start httpd",
       "sudo systemctl enable httpd"
     ]
@@ -68,8 +93,7 @@ connection {
 }
 resource "null_resource" "nullr"{
  
-  depends_on = [aws_volume_attachment.terraebs1, aws_instance.terraos-new1,
-  ]
+  depends_on = [ aws_instance.terraos-new1,aws_efs_mount_target.alpha,]
    
    provisioner "remote-exec" {
 connection {
@@ -79,12 +103,16 @@ connection {
     host     = aws_instance.terraos-new1.public_ip
   }
    inline=[
-   "sudo rm -rf /var/www/html",
-   "sudo mkfs.ext4 /dev/xvdh",
-   "sudo mount /dev/xvdh /var/www/html",
-   "sudo git clone https://github.com/muskan399/first.git /var/www/html",
-   "sudo setenforce 0",
-   "sudo systemctl start httpd"
+     //"sudo su - root",
+     "sudo yum install -y nfs-utils",
+     "sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_mount_target.alpha.dns_name}:/ /var/www/html",
+     "sudo cd /var/www/html",
+     // "sudo su - root",
+           "sudo yum install -y git",
+      "sudo git clone https://github.com/muskan399/first.git /var/www/html",
+     //"echo  My Instance! >> /var/www/html/index.html",
+     "sudo setenforce 0",
+     "sudo systemctl start httpd"
 ]
 
 
@@ -93,27 +121,14 @@ connection {
 output "out1" {
 value=aws_instance.terraos-new1.public_ip
 }
-//Creating EBS volume
-
-resource "aws_ebs_volume" "terraebs-new1" {
-
-  size              = 1
-  tags = {
-    Name = "terra_ebs"
-  }
-availability_zone="${aws_instance.terraos-new1.availability_zone}"
+resource "aws_efs_file_system" "foo" {
+  creation_token = "my-product"
 }
 
-//Attaching EBS volume to myos instance
-
-resource "aws_volume_attachment" "terraebs1" {
-  
-  depends_on = [aws_volume_attachment.terraebs1 ,
-  ]
-  device_name = "/dev/sdh"
-  
-  volume_id   = "${aws_ebs_volume.terraebs-new1.id}"
-  instance_id = "${aws_instance.terraos-new1.id}"
-  force_detach=true
-  
+resource "aws_efs_mount_target" "alpha" {
+  file_system_id = "${aws_efs_file_system.foo.id}"
+  subnet_id      = "${aws_instance.terraos-new1.subnet_id}"
+  security_groups = ["${aws_security_group.terrasg2.id}"]
 }
+
+
